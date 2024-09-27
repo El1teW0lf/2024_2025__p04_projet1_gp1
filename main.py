3ae5597fbd4a4f240c483d27b6f075d33fcb78d1
GoldyRat <grillotcelestin@gmail.com>
2024-09-27 23:29:38 +0200

    Commentaries !

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 def converter(init_number, init_base, target_base):
     bases = ["bin", "dec", "hex"]
-    hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
-    if not init_base in bases:
+    hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"] # Every hexadecimal characters
+    if not init_base in bases: # check if the init base is a base
         return "Please enter a valid init base"
-    if not target_base in bases:
+    if not target_base in bases: # same for the target base
         return "Please enter a valid target base"
     
     for c in init_number:
-        if init_base == "hex" and not c in hex_map:
+        if init_base == "hex" and not c in hex_map: # if every character of the string is a base 16 number and the init base is hex, we can continue
             return "This is not an hexadecimal number"
-        elif not is_number(c):
+        elif not is_number(c): # checking if every character of the string is a number, else the program won't work
             if init_base == "dec":
                 return "This is not a decimal number"
             elif init_base == "bin":
                 return "This is not a binary number"
-        elif init_base == "bin" and int(c) > 1:
+        elif init_base == "bin" and int(c) > 1: # verifying that the number only has 0 or 1, else it's not binary
             return "This is not a binary number"
 
         
@@ -22,13 +22,13 @@ def converter(init_number, init_base, target_base):
     if init_base == target_base:
         return init_number
 
-    
+    # Converting the number to decimal if not already done
     if init_base == "bin":
         init_number = bin_to_dec(init_number)
     elif init_base == "hex":
         init_number = hex_to_dec(init_number)
     
-
+    # If needed, converting the decimal to binary or hexadecimal
     if target_base == "bin":
         return dec_to_bin(init_number)
     elif target_base == "hex":
@@ -39,7 +39,7 @@ def converter(init_number, init_base, target_base):
 
 def is_number(c):
     is_int = False
-    try:
+    try: # if the convertion of the character to an integer isn't working, then it's not a number
         value = int(c)
         is_int = True
     except:
@@ -47,45 +47,45 @@ def is_number(c):
     return is_int
 
 def hex_to_dec(init_number):
-    target_number = ""
-    hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+    
+    hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"] # Every hexadecimal characters
     v1 = []
     for i in init_number:
-        for b in hex_map:
+        for b in hex_map: 
             if i == b:
-                v1.append(hex_map.index(b))
+                v1.append(hex_map.index(b)) # converting the hexadecimal number to a list with all of the coefficients
 
     target_number = 0
     a = len(v1) - 1
     
     for n in v1:
         n = int(n)
-        target_number += n * 16**a
-        a-=1
+        target_number += n * 16**a # Writing the hexadecimal number as a sum of numbers multiplied by the highest possible power of 16
+        a-=1 # Reducing the power as we continue the loop
     
     return target_number
 
 def dec_to_hex(init_number):
     target_number = ""
     n = int(init_number)
-    hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
-    while n > 0:
-        a = n % 16
-        target_number += str(hex_map[a])
-        n //= 16
+    hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"] # Every hexadecimal characters
+    while n > 0: # When the result of the euclidian division of the number by 16 reaches 0, we stop the loop
+        a = n % 16 # The coefficient a is equal to n modulo 16
+        target_number += str(hex_map[a]) # Converting the coefficient a to it's equivalent in hexadecimal and adding it to the target number chain
+        n //= 16 # Dividing the number by 16 while keeping it int as we continue the loop
 
-    target_number = target_number[::-1]
+    target_number = target_number[::-1] # Reversing the loop to get the final number
     return target_number
 
 def dec_to_bin(init_number):
 
     target_number = ""
     n = int(init_number)
-    while n > 0:
-        target_number += str(n % 2)
-        n //= 2
+    while n > 0: # When the result of the euclidian division of the number by 2 reaches 0, we stop the loop
+        target_number += str(n % 2) # Adding the coefficient to the final number
+        n //= 2 # Dividing the number by 2 while keeping it int as we continue the loop
 
-    target_number = target_number[::-1]
+    target_number = target_number[::-1] # Reversing the loop to get the final number
     return target_number
 
 def bin_to_dec(init_number):
@@ -95,8 +95,8 @@ def bin_to_dec(init_number):
     
     for n in init_number:
         n = int(n)
-        target_number += n * 2**a
-        a-=1
+        target_number += n * 2**a # Writing the binary number as a sum of numbers multiplied by the highest possible power of 2
+        a-=1 # Reducing the power as we continue the loop
     
     return target_number
 

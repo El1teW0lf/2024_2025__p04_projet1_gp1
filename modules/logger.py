0c2a8190b86d5b5111ddd5fc3040179630699e66
Hera <162160545+Herasium@users.noreply.github.com>
2024-09-27 17:44:30 +0200

    Update logger.py

---
+++ modules/logger.py
@@ -0,0 +1,4 @@
+#TODO: Finir la function ducoup, la j'ai juste mis qq chose de rapide histoire de pouvoir commencer de mon cote //Victor :)
+
+def LOG(text:str,level:int):
+  print(f"[LOG] {text} (LEVEL: {level})")

b650e0322e31bb00e42f36595974037c5760a2a5
GoldyRat <grillotcelestin@gmail.com>
2024-09-28 12:32:21 +0200

    Fixed issues with 0 and negative numbers as inputs

--- modules/converter.py
+++ modules/converter.py
@@ -6,10 +6,10 @@ from data import DATA
 
 data = DATA()
 
-
+hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
 def converter(init_number, init_base, target_base):
     bases = ["bin", "dec", "hex"]
-    hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+    
 
     if not init_base in bases:
         LOG(data.get_error("INVALID_START_BASE"), 3)
@@ -23,7 +23,7 @@ def converter(init_number, init_base, target_base):
         if init_base == "hex" and not c in hex_map:
             LOG(data.get_error("NOT_HEX_NUMBER"), 3)
             return 0
-        elif not is_number(c):
+        elif not is_natural(c):
             if init_base == "dec":
                 LOG(data.get_error("NOT_DECIMAL_NUMBER"), 3)
                 return 0
@@ -39,33 +39,39 @@ def converter(init_number, init_base, target_base):
     if init_base == target_base:
         return init_number
 
+    if init_number == "0":
+        target_number = "0"
+        return target_number
     
     if init_base == "bin":
-        init_number = bin_to_dec(init_number)
+        target_number = bin_to_dec(init_number)
     elif init_base == "hex":
-        init_number = hex_to_dec(init_number)
+        target_number = hex_to_dec(init_number)
     
 
     if target_base == "bin":
-        return dec_to_bin(init_number)
+        target_number = dec_to_bin(target_number)
     elif target_base == "hex":
-        return dec_to_hex(init_number)
+        target_number = dec_to_hex(target_number)
 
     
-    return str(init_number)
+    return target_number
 
-def is_number(c):
+def is_natural(c):
     is_int = False
     try:
         value = int(c)
         is_int = True
+        if int(c) >= 0:
+            is_int
     except:
         pass
+
     return is_int
 
 def hex_to_dec(init_number):
     target_number = ""
-    hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+    
     v1 = []
     for i in init_number:
         for b in hex_map:
@@ -85,7 +91,7 @@ def hex_to_dec(init_number):
 def dec_to_hex(init_number):
     target_number = ""
     n = int(init_number)
-    hex_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+    
     while n > 0:
         a = n % 16
         target_number += str(hex_map[a])
@@ -119,4 +125,4 @@ def bin_to_dec(init_number):
 
 
 
-print(converter("a10","dec","hex"))
+print(converter("-1","bin","hex"))
